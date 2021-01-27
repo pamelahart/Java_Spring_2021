10d600ee4cdadea1de03e62e8c069187edc90eb5
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-01-26 21:45:21 -0600

    Final Excercise 2 done

--- wk1/Assignments/Part2/Circles1sketch.js
+++ wk1/Assignments/Part2/Circles1sketch.js
@@ -1,12 +1,15 @@
 function setup () {
-  createCanvas(480, 480);
+  createCanvas(380, 1180);
+  background(255, 204, 0);
+  canvas.style = "position:absolute; left: 50%; width: 400px; margin-left: -200px;";
 }
 
 function draw () {
   if (mouseIsPressed) {
-    fill(0);
+    fill(606060);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  quad(mouseX, mouseY, 300, 260);
+
 }
