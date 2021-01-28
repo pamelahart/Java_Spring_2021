8ab0ee1251ae41b25fa70f52f7150d26a0cb7063
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-01-28 06:50:40 -0600

    Create Snowcreature v2

---
+++ JAVA Script _Starter Folder_Add Ons/Project Starter Folder/SnowCreaturev2.js
@@ -0,0 +1,10 @@
+function setup () {
+  createCanvas(480, 120);
+  background(255, 204, 0);
+}
+
+function draw () {
+  background(204);
+  point(240, 260);
+
+}
---
+++ wk1/Assignments/Part3B/SnowCreaturev2.js
@@ -0,0 +1,12 @@
+function setup () {
+  createCanvas(480, 120);
+  background(255, 204, 0);
+}
+
+function draw () {
+  background(204);
+  line(20, 50, 420, 110);
+  ellipse(56, 46, 55, 55);
+  ellipse(120, 100, 110, 110);
+  ellipse(400, 60, 18, 18);
+}
