04ac624a347672d3b5a97cbe42990fefab3c4bd1
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-01-29 00:10:30 -0600

    snowman build

--- wk1/Assignments/Part3B/SnowCreaturev2.js
+++ wk1/Assignments/Part3B/SnowCreaturev2.js
@@ -1,6 +1,8 @@
+
 function setup () {
   createCanvas(820, 820);
   background(255, 204, 0);
+  loadImage("newhead.png");
 }
 
 function draw () {
@@ -8,6 +10,7 @@ function draw () {
   
   
   // Snowman
+ 
   //arms
   strokeWeight(16);
   line(400, 300, 200, 200); 
@@ -15,12 +18,24 @@ function draw () {
   //head
   strokeWeight(4);
   ellipse(450, 150, 120, 120);
+  //eyes
+  stroke(0);
+  strokeWeight(20);
+  point(425, 130);
+  point(475, 130);
+  //mouth
+  strokeWeight(10);
+  arc(430, 157, 90, 40, 0, HALF_PI);
+  //nose
+  point(450, 150);
+  
   //middle
   strokeWeight(6);
   ellipse(450, 300, 220, 220);
   //bottom
   strokeWeight(8);
   ellipse(450, 550, 350, 350);
+  
   
   //horizon
   line(0, 720, 920, 720);
