504b7400a0ecd1d8190aaac3073b88c2e5a9d6eb
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-01-30 10:54:53 -0600

    colors & prompt working

--- wk1/Assignments/Part3B/SnowCreaturev2.js
+++ wk1/Assignments/Part3B/SnowCreaturev2.js
@@ -1,3 +1,4 @@
+let color = window.prompt("list a basic color in lower case", "pink");
 
 function setup () {
   createCanvas(820, 820);
@@ -11,30 +12,44 @@ function draw ()
   // background
   background(51, 192, 255);
 
+  // stripe
+  fill("purple")
+  stroke("purple")
+  rect(0, 300, 820, 250);
+
+
+  
   // Snowman
- fill("#f1f1f1");
+
   //arms
   strokeWeight(16);
   line(400, 300, 200, 200); 
   line(700, 200, 500, 300); 
+ 
   //head
+  fill(color);
   strokeWeight(4);
   ellipse(450, 150, 120, 120);
+  
   //eyes
   stroke(0);
   strokeWeight(20);
   point(425, 130);
   point(475, 130);
+
   //mouth
   strokeWeight(10);
   arc(430, 157, 90, 40, 0, HALF_PI);
+
   //nose
   point(450, 150);
-  
-  //middle
+ 
+  fill("#f1f1f1")
+  //middle circle
   strokeWeight(6);
   ellipse(450, 300, 220, 220);
-  //bottom
+
+  //bottom circle
   strokeWeight(8);
   ellipse(450, 550, 350, 350);
   
@@ -42,10 +57,9 @@ function draw ()
   //horizon
   line(0, 725, 920, 725);
   strokeWeight(4);
+  stroke
 
-  // stripe
-  rect(30, 20, 500, 55);
-  fill("red");
+  
 
  
 
