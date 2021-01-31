4ae9e5faa7d32ae9cdb814dbb01d605882652b92
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-01-31 16:11:44 -0600

    PH Quiz set up1.1

---
+++ wk2/Assignments/PHQuiz1.js
@@ -0,0 +1,6 @@
+const myQuiz = [
+{question: 'color from mixing red and blue', answer: 'purple'},
+{question: 'adding white to a color is', answer: 'tinting'}];
+const firstQuestion = prompt(myQuiz[0].question)
+
+]
