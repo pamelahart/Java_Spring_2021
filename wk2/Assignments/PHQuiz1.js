7e77eaf17aedab7237ae1ce9acdceabb7039cc7f
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-01-31 22:00:09 -0600

    added questions to quiz

--- wk2/Assignments/PHQuiz1.js
+++ wk2/Assignments/PHQuiz1.js
@@ -1,6 +1,12 @@
 const myQuiz = [
-{question: 'color from mixing red and blue', answer: 'purple'},
-{question: 'adding white to a color is', answer: 'tinting'}];
-const firstQuestion = prompt(myQuiz[0].question)
-
-]
+ {question: 'what is your favorite cookie', answer: 'chocolate chip'},
+ {question: 'what is the best burger', answer: 'juicy lucy'},
+ {question: 'what is the best chinese menu item', answer: 'fried rice'},
+ {question: 'what is your favorite cake', answer: 'carrot'},
+];
+const firstQuestion = prompt(myQuiz[0].question);
+if(firstQuestion == myQuiz[0].answer) {
+    alert('that is absolutely correct');
+} else {
+    alert('no that is not correct at all');
+}   
