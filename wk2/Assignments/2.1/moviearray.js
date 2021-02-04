84abe4cdccbd2e11e27ae0a0f668232fbb307722
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-04 07:54:03 -0600

    movie array push

--- wk2/Assignments/2.1/moviearray.js
+++ wk2/Assignments/2.1/moviearray.js
@@ -1,15 +1,38 @@
-const myArr = 
+const myMovieArr = 
 ['Star Wars', 'The Italian Job', 'Iron Man', 'The Avengers', 
  'Raiders of the Lost Ark'];
+const myMovieQuiz = window.prompt('What is your favorite movie');
+myMovieArray.push('Tomb Raider'); // add item to end
 
-const myOtherMovieArray = new Array();
-myOtherMovieArray.push('Tomb Raider'),
-console.log(myArr);
-console.log(myOtherMovieArray);
+console.log(myMovieArr);
+console.log(myMovieArray);
 
-//const myMovies = [
-//    { question: 'what is your favorite movie', answer: 'Star Wars' },
-//];
+
+//Step:
+//create an array with the title of your favorite movie titles 
+//prompt the user to enter their favorite movie.Use the prompt() method to do this.
+//add the movie to the array using push or create a new array with concat
+//display full list of movies with toString() method
+//Update your github repo with this exercise and then upload your exercise to your MCAD hosting and share a link to that url in this discussion.Ensure that URL is accessible.
+
+//myArr.pop(); //removes last item
+//myArr.shift(); //removes first item
+//myArr.unshift(); //adds to the beginning of item
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //const firstQuestion = prompt(myArr[0].question);
 //if(firstQuestion == myArr[0].answer) {
