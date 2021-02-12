377b7cda48650036bae1734d5a199163a03ad794
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-12 00:24:57 -0600

    Update Clicker4.2.js
    Initial setup for 3.3

--- wk4/4.2/Clicker4.2.js
+++ wk4/4.2/Clicker4.2.js
@@ -1,18 +1,24 @@
 let rectX = 0;
-let rectWidth = 75;
+const rectWidth = 75;
+let clickCount = 0;
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw () {
-  background(155);
+  background('#afeeee');
   drawShape();
   rectX++;
+  if (rectX > width) {
+    noLoop();
+    text('Your score was ' + clickCount, 100, 300);}
 }
 
 function mousePressed() {
-  if ((mouseX >= rectX && mouseX <= 75) && (mouseY >= 0 && mouseY >= 0 && mouseY <= 75)) {
-    console.log('hit');
+  if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= 0 && mouseY >= 0 && mouseY <= 75)) {
+    clickCount++;
+    console.log('hit', clickCount);
+  
   }
 }
 
