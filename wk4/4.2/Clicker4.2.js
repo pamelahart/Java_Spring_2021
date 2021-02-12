8ea29eb29ef6e32ac534708e3e78b9fe27a77407
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-11 23:23:50 -0600

    4.2 setup

---
+++ wk4/4.2/Clicker4.2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500);
+}
+function draw () {
+  
+}
+//Create a new p5.js project.  In the setup function, create a 500 x 500 canvas. In the draw function, create a shape (square or ellipse) and use one of the methods described in either the p5.js example or the reading from the project to determine if the use has clicked on the shape. Use this event handler to increment a variable.
+
+//After you have that working, create a variable to be used for either the shapes x or y.Increment this in the draw function so the shape appears to be moving.This should still detecting mouse clicks.
+
+//After you have this working, create a conditional in your draw function to detect if the shape is off the canvas.When this occurs, call 'noLoop' to stop the draw function from repeating.Display the number of clicks the user has done.
+
+//Feel free to make this interesting with adding color and description text.
