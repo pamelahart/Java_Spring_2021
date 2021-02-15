dae359a26e1d17b490e7ed5bfdc296fcdc4c9cad
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-14 21:54:36 -0600

    edits

--- wk4/4.3/MemoryGame.js
+++ wk4/4.3/MemoryGame.js
@@ -8,8 +8,9 @@ let myRectangle = [];
 function setup() {
   createCanvas(1000, 1500);
   background (0);
+  fill('#3e8e6e');
   for (let rows = 0; rows < 4; rows++) {
-    for (let columns = 0; columns < 4; columns++) {
+    for (let columns = 0; columns < 3; columns++) {
       rect(startingX, startingY)
       myRectangle.push({ x: startingX, y: startingY });
       startingX += 150;
