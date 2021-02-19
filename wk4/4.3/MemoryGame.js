19fd87bcedcef8062f2085a417cfa74a5df9c272
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-18 22:07:51 -0600

    edits

--- wk4/4.3/MemoryGame.js
+++ wk4/4.3/MemoryGame.js
@@ -1,22 +1,23 @@
-let rectX = 0;
-const rectHeight = 300;
+let rectX = 0; // let statements can be changed by a function or reassignment 
+const rectHeight = 300; // must remain the same throughout- can't be changed regardless
 const rectWidth = 200;
-let startingX = 200;
-let startingY = 100;
+let startingX = 50;
+let startingY = 50;
 let myRectangle = [];
 
 function setup() {
-  createCanvas(1500, 1500);
+  createCanvas(1000, 1000); // make smaller
+
   background('#ffffcc'); 
   fill('#141b30');
-  for (let rows = 0; rows < 3; rows++) {
+  for (let rows = 0; rows < 3; rows++) { // varible (temp only available to this loop), condition (the true false statement. stops when the statement becomes false) , modifer ()... +=1 
     for (let columns = 0; columns < 4; columns++) {
       rect(startingX, startingY, rectWidth, rectHeight);
       myRectangle.push({ x: startingX, y: startingY });
       startingX += 225;
 }
 startingY +=325;
-startingX = 200;
+startingX = 50;
   }
   console.log(myRectangle);
 }
--- wk5/timers.js
+++ wk5/timers.js
@@ -1,5 +1,11 @@
 function setup () {
-createCanvas(500,500)
+createCanvas(500, 500);
 background(0);
+drawBlock();
+}
+
+function drawBlock(x, y, color) {
+fill(color || 255);
+rect(x, y, 50, 50);
 
 }
