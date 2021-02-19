591324142e1f4ea693c0615217b9c0cff7abd03e
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-18 22:58:22 -0600

    edits

--- wk5/timers.js
+++ wk5/timers.js
@@ -1,11 +1,19 @@
+let blockX = 0;
+let blockY = 0;
+let drawTimer; 
+let speed = 50;
+
 function setup () {
-createCanvas(500, 500);
-background(0);
-drawBlock();
+    createCanvas(500, 500);
+    background(0);
+
 }
 
 function drawBlock(x, y, color) {
-fill(color || 255);
-rect(x, y, 50, 50);
-
+    fill(color || 255);
+    rect(x, y, 50, 50);
 }
+
+drawTimer = window.setInterval(() => {
+    drawBlock(blockX, blockY, 255);
+}, speed);
