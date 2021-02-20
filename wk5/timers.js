a56ffe1f6860ced8eefea96fe8f3200758157830
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-20 08:15:54 -0600

    edits & box movement working w/ prompt box appearing- boxes blue, issues w/ small movement, pressing key not working corectly

--- wk5/timers.js
+++ wk5/timers.js
@@ -1,5 +1,6 @@
 let blockX = 0;
 let blockY = 0;
+let blockColor = 255;
 let drawTimer; 
 const speed = 10;
 const distance = 2;
@@ -7,20 +8,25 @@ const distance = 2;
 function setup () {
     createCanvas(500, 500);
     background(0);
-
 }
 function drawBlock(x, y, color) {
     fill(color || 255);
     rect(x, y, 50, 50);
 }
 function keyTyped() {
-    console.log('you pressed', key);
+    let keyToNumber = Number(key);
+    if (isNaN(keyToNumber)){
+        return;
+    }
+    keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+    console.log('converted number', keyToNumber);
+    blockColor = keyToNumber;
 }
 
 window.setTimeout(() => {
     drawTimer = window.setInterval(() => {
         if(blockY -50 <= height) {
-            drawBlock(blockX, blockY, 255);
+            drawBlock(blockX, blockY, blockColor);
             blockY += distance;
         } else {
         blockY = 0;
