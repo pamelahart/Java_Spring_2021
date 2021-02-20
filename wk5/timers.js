0fea70702703b94b36339fc96d7cba2079af1d1e
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-19 21:47:46 -0600

    working again- start here if needed

--- wk5/timers.js
+++ wk5/timers.js
@@ -9,24 +9,19 @@ function setup () {
     background(0);
 
 }
+
 function drawBlock(x, y, color) {
     fill(color || 255);
     rect(x, y, 50, 50);
 }
-function keyType() {
-    let KeyToNumber = Number(key)
-    console.log('key to number3', keyToNumber);   
-
-}
 
 drawTimer = window.setInterval(() => {
-    if (blockY -50 <= height) {
+    if(blockY -50 <= height) {
     drawBlock(blockX, blockY, 255);
     blockY += distance;
     } else {
         blockY = 0;
         blockX += 50;
     }
-    if (blockY) - 50 > height && blockX - 50 > width)
-        window.clearInterval(drawTimer);
+
 }, speed);
