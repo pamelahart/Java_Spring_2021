b83408a2a3ce2ed6c539276a3098f6ddaf97995a
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-21 13:49:25 -0600

    added card

--- wk5/5.3/MemoryGame2.js
+++ wk5/5.3/MemoryGame2.js
@@ -21,3 +21,15 @@ startingX = 50;
   }
   console.log(myRectangle);
 }
+class Card {
+  constructor() {
+    this.x = 100;
+    this.y = 100;
+    this.width = 80;
+    this.height = 100;
+  }
+  show() {
+    fill('ffffcc')
+    rect(this.x, this.y, this.width, this.height, 10);
+
+  }
