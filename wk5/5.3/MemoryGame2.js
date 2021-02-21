4c836231f926c3174c9b76bdd0f02c686d576366
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-21 14:27:58 -0600

    edits

--- wk5/5.3/MemoryGame2.js
+++ wk5/5.3/MemoryGame2.js
@@ -5,6 +5,8 @@ let startingX = 55;
 let startingY = 175;
 let myRectangle = [];
 let myCard;
+const DOWN = 'down';
+const UP = 'up';
 
 function setup() {
   createCanvas(1000, 1200); // make title, instructions & score fit within the canvas
@@ -24,7 +26,7 @@ function setup() {
   console.log(myRectangle);
 }
 function mousePressed(){
-  myCard.didHit(mouseX, mouseY);
+  console.log(myCard.didHit(mouseX, mouseY));
 }
 
 class Card {
@@ -33,10 +35,25 @@ class Card {
     this.y = 100;
     this.width = 80;
     this.height = 100;
+    this.face = DOWN;
     this.show();
   }
   show () {
     fill('ffffcc');
     rect(this.x, this.y, this.width, this.height, 10);
   }
-}
+  didHit (mouseX, mouseY) {
+    if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
+        return true;
+    } else {
+      return false;
+    }
+  } 
+  flip () {
+    if (this.face === DOWN) {
+        this.face = UP;
+    } else {
+      this.face = DOWN;
+    }
+  }
+} 
