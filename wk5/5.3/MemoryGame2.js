b25f64c8ab4dde845e80a0b1dcbd0dfb71cdd88a
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-21 21:08:04 -0600

    new flip

--- wk5/5.3/MemoryGame2.js
+++ wk5/5.3/MemoryGame2.js
@@ -1,33 +1,39 @@
-/* let rectX = 0; // let statements can be changed by a function or reassignment 
-const rectHeight = 300; // must remain the same throughout- can't be changed regardless
-const rectWidth = 200;
-
-//let myRectangle (); */
-
-let myCard;
+let cards = [];
 const DOWN = 'down';
 const UP = 'up';
-let startingX = 55;
-let startingY = 175;
-const gameState = {
-  
-}
+let startingX = 55
+let startingY = 175
+const rectHeight = 300; // must remain the same throughout- can't be changed regardless
+const rectWidth = 200;
 
 function setup() {
   createCanvas(1000, 1200); // make title, instructions & score fit within the canvas
   background('#8ED8F8'); // ffffcc
-  for (let i = 0; i < 5; i++);
-  //fill('#141b30');
-
+  for (let rows = 0; rows < 3; rows++) { // varible (temp only available to this loop), condition (the true false statement. stops when the statement becomes false) , modifer ()... +=1 
+    for (let columns = 0; columns < 4; columns++) {
+      cards.push(new Card(startingX, startingY));
+      startingX += 225;
+    }
+    startingY += 325;
+    startingX = 50;
+  }
 }
 function mousePressed(){
-  console.log(myCard.didHit(mouseX, mouseY));
+//console.log(myCard.didHit(mouseX, mouseY));
+  for ( let cardsArray_index = 0; cardsArray_index < cards.length; cardsArray_index++) {
+    let tempCard = cards[cardsArray_index];
+    if (tempCard.didHit(mouseX, mouseY)) {
+      tempCard.flip()
+      console.log (tempCard)
+    }
+
+  }
 }
 
 class Card {
-  constructor() {
-    this.x = 100;
-    this.y = 100;
+  constructor(x, y ) {
+    this.x = x;
+    this.y = y;
     this.width = 200;
     this.height = 300;
     this.face = DOWN;
@@ -36,7 +42,7 @@ class Card {
 
   show () {
     if(this.face === DOWN) {
-        fill('ffffcc');
+      fill('#141b30');
         rect(this.x, this.y, this.width, this.height, 10);
     } else {
       fill('#aaa');
@@ -61,13 +67,3 @@ class Card {
   }
 } 
 
- // for (let rows = 0; rows < 3; rows++) { // varible (temp only available to this loop), condition (the true false statement. stops when the statement becomes false) , modifer ()... +=1 
- //  for (let columns = 0; columns < 4; columns++) {
- //     rect(startingX, startingY, rectWidth, rectHeight);
- //     myRectangle.push({ x: startingX, y: startingY });
- //     startingX += 225;
- // }
- // startingY +=325;
- // startingX = 50;
- // }
- // console.log(myRectangle);
