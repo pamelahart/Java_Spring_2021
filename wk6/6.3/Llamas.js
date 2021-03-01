4d581b4d00945b5e0930696ad80ed05dfeb02931
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-02-28 23:22:34 -0600

    js setup

---
+++ wk6/6.3/Llamas.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#myApp",
+    data: {
+        llamaFact1: "Llamas are smart and easy to train.",
+        llamaFact2: "spit when they're agitated.",
+        llamaFact3: "Llamas are vegetarians.",
+        llamaFact4: "Llamas are social animals who live in herds",
+        llamaFact5: "There are more than 7 million llamas.",
+        llamaFact6: "Llama Yarn is soft, lightweight and remarkably warm.",
+        llamaFact7: "Llamas can live to be 30 years old.",
+        llamaFact8: "Llamas have ears that stand straight up",
+        llamaFact9: "Llamas are sure-footed and easily navigating rocky terrain.",
+        llamaFact10: "Llamas have been used as guard animals for livestock."
+    }
+});
