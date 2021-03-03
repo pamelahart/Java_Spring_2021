571c57d8352af8902db2ed9c426c1132b1ce3357
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-03-03 07:57:47 -0600

    edits to css & html

--- wk6/6.3/Llamas.js
+++ wk6/6.3/Llamas.js
@@ -1,29 +1,16 @@
 new Vue({
     el: "#myApp",
     data: {
-        llamaFact1: "Llamas are smart and easy to train",
-        llamaFact2: "spit when they're agitated.",
-        llamaFact3: "Llamas are vegetarians.",
-        llamaFact4: "Llamas are social animals who live in herds",
-        llamaFact5: "There are more than 7 million llamas.",
-        llamaFact6: "Llama Yarn is soft, lightweight and remarkably warm.",
-        llamaFact7: "Llamas can live to be 30 years old.",
-        llamaFact8: "Llamas have ears that stand straight up",
-        llamaFact9: "Llamas are sure-footed and easily navigating rocky terrain.",
-        llamaFact10: "Llamas have been used as guard animals for livestock.",
+        llamaFact1: "1. Are smart and easy to train",
+        llamaFact2: "2. Spit when they're agitated.",
+        llamaFact3: "3. They are vegetarians.",
+        llamaFact4: "4. Are social animals who live in herds",
+        llamaFact5: "5. There are more than 7 million llamas.",
+        llamaFact6: "6. Llama yarn is soft, lightweight & warm.",
+        llamaFact7: "7. Can live to be 30 years old.",
+        llamaFact8: "8. Their ears that stand straight up",
+        llamaFact9: "9. Llamas are sure-footed and easily navigating rocky terrain.",
+        llamaFact10: "10. Llamas have been used as guard animals for livestock.",
         llamaFriendly: true
-    
     }
 });
-
-    
-    /*  llamaFact1: "Llamas are smart and easy to train.",
-    llamaFact2: "spit when they're agitated.",
-    llamaFact3: "Llamas are vegetarians.",
-    llamaFact4: "Llamas are social animals who live in herds",
-    llamaFact5: "There are more than 7 million llamas.",
-    llamaFact6: "Llama Yarn is soft, lightweight and remarkably warm.",
-    llamaFact7: "Llamas can live to be 30 years old.",
-    llamaFact8: "Llamas have ears that stand straight up",
-    llamaFact9: "Llamas are sure-footed and easily navigating rocky terrain.",
-    llamaFact10: "Llamas have been used as guard animals for livestock." */
