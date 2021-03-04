4534d7a3cbff4aadcd2f31cd73a2f82d0638b7a3
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-03-03 21:49:31 -0600

    edits done.

--- wk6/6.3/Llamas.js
+++ wk6/6.3/Llamas.js
@@ -5,12 +5,12 @@ new Vue({
         llamaFact2: "2. Spit when they're agitated.",
         llamaFact3: "3. They are vegetarians.",
         llamaFact4: "4. Are social animals who live in herds",
-        llamaFact5: "5. There are more than 7 million llamas.",
-        llamaFact6: "6. Llama yarn is soft, lightweight & warm.",
+        llamaFact5: "5. There are more than 7 million worldwide.",
+        llamaFact6: "6. Yarn is soft, lightweight & warm.",
         llamaFact7: "7. Can live to be 30 years old.",
-        llamaFact8: "8. Their ears that stand straight up",
-        llamaFact9: "9. Llamas are sure-footed and easily navigating rocky terrain.",
-        llamaFact10: "10. Llamas have been used as guard animals for livestock.",
+        llamaFact8: "8. Ears stand straight up",
+        llamaFact9: "9. Sure-footed to navigate rocky terrain.",
+        llamaFact10: "10. Used as guard animals for livestock.",
         llamaFriendly: true
     }
 });
