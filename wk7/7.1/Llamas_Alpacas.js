001187520d70d302efe7b6c35975d0c1741f4c0e
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-03-06 08:10:54 -0600

    Attribute Binding- My link not working, neither is cloaking

---
+++ wk7/7.1/Llamas_Alpacas.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        LlamaVsAlpaca1: https://en.wikipedia.org/wiki/Alpaca,
+
+    }
+});
+
+// What are mustaches???? same as curly brackets? why are they now called this?
+//<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
---
+++ wk7/7.3/Llamas.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#myApp",
+    data: {
+        llamaFact1: "1. Are smart and easy to train",
+        llamaFact2: "2. Spit when they're agitated.",
+        llamaFact3: "3. They are vegetarians.",
+        llamaFact4: "4. Are social animals who live in herds",
+        llamaFact5: "5. There are more than 7 million worldwide.",
+        llamaFact6: "6. Yarn is soft, lightweight & warm.",
+        llamaFact7: "7. Can live to be 30 years old.",
+        llamaFact8: "8. Ears stand straight up",
+        llamaFact9: "9. Sure-footed to navigate rocky terrain.",
+        llamaFact10: "10. Used as guard animals for livestock.",
+        llamaFriendly: true
+    }
+});
