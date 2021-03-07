d4e03a670942adafcc338dbff09712d9ff91d2d7
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-03-07 17:06:08 -0600

    set up for 7.3

---
+++ wk7/7.3/cookie.js
@@ -0,0 +1,10 @@
+/* new Vue({
+    el: "#app",
+    data: {
+        alpaca: "https://en.wikipedia.org/wiki/Alpaca",
+        llama: "https://en.wikipedia.org/wiki/Llama"
+    }
+}); */
+
+// What are mustaches???? same as curly brackets? why are they now called this?
+//<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
