82e194ad08b16c42607090e992f4de8e41203cb8
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-03-07 20:08:46 -0600

    clean up JS

--- wk7/7.3/cookie.js
+++ wk7/7.3/cookie.js
@@ -1,52 +1,47 @@
-/* new Vue({
-    el: "#app",
+let vm = new VTTCue({
+    el: "#myApp",
     data: {
-        alpaca: "https://en.wikipedia.org/wiki/Alpaca",
-        llama: "https://en.wikipedia.org/wiki/Llama"
-    }
-}); */
+        cookies: [
+        {
+            name: 'Chocolate Chip',
+            prep: '15 min',
+            time: '1 hour 30 minutes',
+            yields: '48',
+            recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl,   beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla. Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight. Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes.Cool slightly before serving."
+            }, {
 
-// What are mustaches???? same as curly brackets? why are they now called this?
+                name: 'Peanut Butter',
+                prep: '15 min',
+                time: '1 hour 30 minutes',
+                yields: '48',
+                recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl,beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla. Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight. Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes.Cool slightly before serving."
+            }, {
+                name: 'Macroon',
+                prep: '15 min',
+                time: '1 hour 30 minutes',
+                yields: '48',
+                recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl,beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla. Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight. Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes.Cool slightly before serving."
+            }, {
+                name: 'Ginger',
+                prep: '15 min',
+                time: '1 hour 30 minutes',
+                yields: '48',
+                recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl,beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla. Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight. Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes.Cool slightly before serving."
+            }, {
+                name: 'Double Chocolate',
+                prep: '15 min',
+                time: '1 hour 30 minutes',
+                yields: '48',
+                recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl,beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla. Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight. Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes.Cool slightly before serving."
+            }
+        }
+    })
+
+    // What are mustaches???? same as curly brackets? why are they now called this?
 //<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
-draw()
+/* function setup() {
+    createCanvas(1000, 1000); // make smaller
 
-{
-    name: 'Chocolate Chip',
-    prep: 15 min,
-    time: '1 hour 30 minutes',
-    yields: '48',
-    recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl, beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla.Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight.
-    Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes. Cool slightly before serving."
-}
-{
-    name: 'Peanut Butter',
-    prep: 15 min,
-    time: '1 hour 30 minutes',
-    yields: '48',
-    recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl, beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla.Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight.
-    Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes. Cool slightly before serving."
-}
-{
-    name: 'Macroon',
-    prep: 15 min,
-    time: '1 hour 30 minutes',
-    yields: '48',
-    recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl, beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla.Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight.
-    Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes.Cool slightly before serving."
-}
-{
-    name: 'Double Chocolate',
-    prep: 15 min,
-    ime: '1 hour 30 minutes',
-    yields: '48',
-    recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl, beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla.Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight.
-    Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes. Cool slightly before serving."
-}
-{
-    name: 'Ginger',
-    prep: 15 min,
-    time: '1 hour 30 minutes',
-    yields: '48',
-    recipe: "Whisk together flour, cocoa powder, baking soda, and salt in a bowl; set aside. In a large bowl, beat butter and sugars on medium speed of a hand mixer until fluffy, about 3 minutes.Add eggs one at a time, beating well after each addition until smooth; beat in vanilla.Add dry ingredients, and beat until just combined; stir in chocolate chips.Roll cookies into 4 - ounce balls and place on a parchment paper - lined baking sheet about 4 - inches apart.Refrigerate for 4 hours or up to overnight.
-    Heat oven to 350°. Bake cookies, rotating once halfway through, about 15 minutes. Cool slightly before serving."
-}
+    background('#ffffcc');
+/*     fill('#141b30'); */
+/* draw() */
